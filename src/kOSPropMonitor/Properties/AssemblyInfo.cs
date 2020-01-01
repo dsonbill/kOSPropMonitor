9012deaebb6c405d4039c4665ec41bd4f93b04cf
Jon Cable <jonnyothan@gmail.com>
2020-01-01 09:43:17 -0800

    update for 1.8

--- src/kOSPropMonitor/Properties/AssemblyInfo.cs
+++ src/kOSPropMonitor/Properties/AssemblyInfo.cs
@@ -14,9 +14,9 @@ using System.Runtime.CompilerServices;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("1.5.1.*")]
+[assembly: AssemblyVersion ("1.8.1.*")]
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
-[assembly: KSPAssemblyDependency("kOS", 1, 0)] // <<-- This is important!!!
+[assembly: KSPAssemblyDependency("kOS", 1, 2)] // <<-- This is important!!!
